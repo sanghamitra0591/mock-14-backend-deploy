66aeb04b675aded7dfca08f3ca3d8a447e48e735
sanghamitra0591 <sanghamitramymail@gmail.com>
2023-03-04 12:55:50 +0530

    initiated

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const { userRouter } = require("./routes/user.route");
 
 const app= express();
 
+const cors= require("cors");
+
+app.use(cors({
+    origin : "*"
+}))
+
 app.use(express.json());
 
 require("dotenv").config();
